c14c0c90ab63f4f923b523d221e1961c1cfccbbf
astur <astur@yandex.ru>
2018-04-07 23:43:22 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = () => {};
+module.exports = arr => {
+    arr = arr.slice().sort((x, y) => x - y);
+    const len = arr.length;
+    const sum = arr.reduce((a, b) => a + b, 0);
+    return {
+        min: arr[0],
+        max: arr[len - 1],
+        sum,
+        len,
+        avg: sum / len,
+    };
+};
