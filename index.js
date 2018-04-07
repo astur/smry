93a0c317e9ac19997a3b147c745ba53583d9679a
astur <astur@yandex.ru>
2018-04-08 01:07:06 +0300

    'fractionDigits' option added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-module.exports = (arr, {sorted = false} = {}) => {
+module.exports = (arr, {
+    sorted = false,
+    fractionDigits = 10,
+} = {}) => {
     if(!sorted) arr = arr.slice().sort((x, y) => x - y);
     const len = arr.length;
     const sum = arr.reduce((a, b) => a + b, 0);
-    return {
+    const r = {
         min: arr[0],
         max: arr[len - 1],
         sum,
         len,
         avg: sum / len,
     };
+    Object.keys(r).forEach(k => {
+        r[k] = +r[k].toFixed(fractionDigits);
+    });
+    return r;
 };
