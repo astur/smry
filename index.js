ed44cd197977cc91f4dc75b5e2700f063a054e2b
astur <astur@yandex.ru>
2018-04-08 03:17:01 +0300

    param checking added

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const _quantile = require('quantile');
 const arfy = require('arfy');
+const type = require('easytype');
 
 module.exports = (arr, {
     sorted = false,
     fractionDigits = 10,
     quantile = [],
 } = {}) => {
-    if(!sorted) arr = arr.slice().sort((x, y) => x - y);
+    if(!type.isArray.ofNumbers(arr) || !arr.length){
+        throw new TypeError('Expected array of numbers');
+    }
+    if(!type.isNumber(fractionDigits)){
+        throw new TypeError('Expected fractionDigits is number');
+    }
+    if(fractionDigits > 20 || fractionDigits < 0){
+        throw new RangeError('Expected fractionDigits is from 0 to 20');
+    }
+
     quantile = arfy(quantile);
+    if(!type.isArray.ofNumbers(quantile)){
+        throw new TypeError('Expected quantile is number or array of numbers');
+    }
+    if(quantile.some(q => q <= 0 || q >= 1)){
+        throw new RangeError('Expected fractionDigits is between 0 and 1');
+    }
+
+    if(!sorted) arr = arr.slice().sort((x, y) => x - y);
     const len = arr.length;
     const sum = arr.reduce((a, b) => a + b, 0);
     const r = {
