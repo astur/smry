b1628fa9b66963d20222a886c9f03b10bc0eb356
astur <astur@yandex.ru>
2018-08-19 14:47:52 +0300

    code linted

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const _quantile = require('quantile');
 const arfy = require('arfy');
 const type = require('easytype');
 
-module.exports = (arr, {
+module.exports = (a, {
     sorted = false,
     fractionDigits = 10,
     quantile = [],
 } = {}) => {
-    if(!type.isArray.ofNumbers(arr) || !arr.length){
+    if(!type.isArray.ofNumbers(a) || a.length === 0){
         throw new TypeError('Expected array of numbers');
     }
     if(!type.isNumber(fractionDigits)){
@@ -17,15 +17,16 @@ module.exports = (arr, {
         throw new RangeError('Expected fractionDigits is from 0 to 20');
     }
 
-    quantile = arfy(quantile);
-    if(!type.isArray.ofNumbers(quantile)){
+    const q = arfy(quantile);
+    if(!type.isArray.ofNumbers(q)){
         throw new TypeError('Expected quantile is number or array of numbers');
     }
-    if(quantile.some(q => q <= 0 || q >= 1)){
+    if(q.some(q => q <= 0 || q >= 1)){
         throw new RangeError('Expected fractionDigits is between 0 and 1');
     }
 
-    if(!sorted) arr = arr.slice().sort((x, y) => x - y);
+    const arr = sorted ? a : a.slice().sort((x, y) => x - y);
+
     const len = arr.length;
     const sum = arr.reduce((a, b) => a + b, 0);
     const r = {
@@ -38,7 +39,7 @@ module.exports = (arr, {
     Object.keys(r).forEach(k => {
         r[k] = +r[k].toFixed(fractionDigits);
     });
-    quantile.forEach(q => {
+    q.forEach(q => {
         if(!r.quantile) r.quantile = {};
         r.quantile[q] = +_quantile(arr, q).toFixed(fractionDigits);
     });
